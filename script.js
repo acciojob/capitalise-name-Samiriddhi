d3a7332bb028a15b09cbd85297e0df2c658b8676
Samriddhi Shreyasee <112820579+Samiriddhi@users.noreply.github.com>
2024-09-17 00:01:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let nameInput = document.getElemenById("fname");
+
+
+nameInput.addEventListener("blur" , ()=>{
+	nameInput.value = nameInput.value.toUpperCase();
+})
